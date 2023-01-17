18ce0f8d80e9a80723050ed93dcc41767639f94e
bryanjamesl <bryanjamesl@gmail.com>
2023-01-17 13:29:51 -0700

    Added loop for variation - three 50ms, one 150ms

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,17 @@ pinMode(onboardLED, OUTPUT);            // on board LED pin for reference
 
 void loop() 
 {
+    for(int i = 0; i < 3; i++ )
+    {
     digitalWrite(extLED, HIGH);         // turn on LED bars
     digitalWrite(onboardLED, HIGH);     // turn on on board LED
     delay(50);                          // wait 50ms
     digitalWrite(extLED, LOW);          // turn off LED bars
     digitalWrite(onboardLED, LOW);      // turn off on board LED
     delay(50);                          // wait 50ms
-      
+    }
+    digitalWrite(onboardLED, HIGH);
+    delay(150);
+    digitalWrite(onboardLED, LOW); 
+    delay(150);
 }
